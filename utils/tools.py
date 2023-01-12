288fecf4c0de41c9c11f84d3c5685deee231fbd5
Jun-yong-lee <wnsdyd1569@naver.com>
2023-01-13 02:06:18 +0900

    code main.py & yolov3.py & tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -2,6 +2,34 @@ import numpy as np
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 
+# parse model configuration
+def parse_model_config(path):
+    file = open(path, 'r')
+    lines = file.read().split('\n')
+    lines = [x for x in lines if x and not x.startswith('#')]
+    lines = [x.rstrip().lstrip() for x in lines]
+        
+    module_defs = []
+    type_name = None
+    for line in lines:
+        if line.startswith('['):
+            type_name = line[1:-1].rstrip()
+            if type_name == "net":
+                continue
+            module_defs.append({})        
+            module_defs[-1]['type'] = type_name
+            if module_defs[-1]['type'] == 'convolutional':
+                module_defs[-1]['batch_normalize'] = 0
+        else:
+            if type_name == "net":
+                continue
+            key, value = line.split("=")
+            value = value.strip()
+            module_defs[-1][key.rstrip()] = value.strip()
+
+    return module_defs
+    
+
 # parse the yolov3 configuration
 def parse_hyperparm_config(path):
     file = open(path, 'r')
