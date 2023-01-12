388bb543aeb2d80a845d2571b59952491578b25d
Jun-yong-lee <wnsdyd1569@naver.com>
2023-01-13 00:35:37 +0900

    class AbsoluteLabels()

---
+++ dataloader/data_transforms.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+import torch
+from torchvision import transforms as tf
+
+import imgaug as ia
+from imgaug import augmenters as iaa
+from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
+
+def get_transformations(cfg_param = None, is_train = None):
+    if is_train:
+        data_transform = tf.Compose([AbsoluteLabels()])
+    else:
+        data_transform = tf.Compose([AbsoluteLabels()]) 
+    return data_transform
+
+# absolute bbox
+class AbsoluteLabels(object):
+    def __init__(self,):
+        pass
+
+    def __call__(self, data):
+        image, label = data
+        h, w, _ = image.shape
+        label[:, [1, 3]] *= w # cx, w
+        label[:, [2, 4]] *= h # cy, h
+        return image, label
