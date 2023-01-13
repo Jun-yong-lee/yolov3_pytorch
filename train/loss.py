3b184afc2569155f8bbbd33cf3356cba21173ea0
Jun-yong-lee <wnsdyd1569@naver.com>
2023-01-13 15:56:48 +0900

    finding error

--- train/loss.py
+++ train/loss.py
@@ -12,24 +12,38 @@ class Yololoss(nn.Module):
     def compute_loss(self, pred, targets, yololayer):
         lcls, lbox, lobj = torch.zeros(1, device=self.device), torch.zeros(1, device=self.device), torch.zeros(1, device=self.device)
         
+        # pout.shape : [batch, anchors, grid_y, grid_x, box_attrib]
+        # the number of boxes in each yolo layer : anchors * grid_h * grid_w
+        # yolo0 -> 3 * 19 * 19, yolo1 -> 3 * 38 *38, yolo2 -> 3 * 76 * 76
+        # total boxes : 22,743.
+        # positive prediction vs negative prediction
+        # pos : neg = 0.01 : 0.99
+        # Only in positive prediction, we can get box_loss and class_loss
+        # in negative prediction, only obj_loss
+        
         # get positive targets
-        self.get_targets(pred, targets, yololayer)
+        tcls, tbox, tindices, tanchors = self.get_targets(pred, targets, yololayer)
         
         
         # 3 yolo layers
         for pidx, pout in enumerate(pred):
-            print(f"yolo {pidx}, shape {pout.shape}")
+            batch_id, anchor_id, gy, gx = tindices[pidx]
             
-            # pout.shape : [batch, anchors, grid_y, grid_x, box_attrib]
-            # the number of boxes in each yolo layer : anchors * grid_h * grid_w
-            # yolo0 -> 3 * 19 * 19, yolo1 -> 3 * 38 *38, yolo2 -> 3 * 76 * 76
-            # total boxes : 22,743.
-            # positive prediction vs negative prediction
-            # pos : neg = 0.01 : 0.99
-            # Only in positive prediction, we can get box_loss and class_loss
-            # in negative prediction, only obj_loss
+            tobj = torch.zeros_like(pout[...,0], device=self.device)
             
-    def get_targets(self, preds, targets, yolo_layer):
+            num_targets = batch_id.shape[0]
+            
+            if num_targets:
+                ps = pout[batch_id, anchor_id, gy, gx] # [batch, anchor, grid_h, grid_w, box_attrib]
+                pxy = torch.sigmoid(ps[...,0:2])
+                pwh = torch.exp(ps[2:4]) * tanchors[pidx]
+                pbox = torch.cat((pxy, pwh), 1)
+                print(pbox)
+
+            
+
+            
+    def get_targets(self, preds, targets, yololayer):
         num_anc = 3
         num_targets = targets.shape[0]
         tcls, tboxes, indices, anch = [], [], [], []
@@ -41,12 +55,46 @@ class Yololoss(nn.Module):
         # targets shape : [batch_id, class_id, box_cs, box_cy, box_w, box_h, anchor_id]
         targets = torch.cat((targets.repeat(num_anc, 1, 1), ai[:, :, None]), 2)
         
-        for yi, yl in enumerate(yolo_layer):
+        for yi, yl in enumerate(yololayer):
             anchors = yl.anchor / yl.stride
-            print(anchors)
             
             gain[2:6] = torch.tensor(preds[yi].shape)[[3,2,3,2]] # grid_w, grid_h / ex) 0.1 -> 16.2 real box location
             
             t = targets * gain
             
-            print(t)
+            if num_targets:
+                
+                r = t[:, :, 4:6] / anchors[:, None]
+                
+                # select thresh ratios less than 4
+                j = torch.max(r, 1./r).max(2)[0] < 4
+                
+                t = t[j]
+            else:
+                t = targets[0]
+            
+            # batch_id, class_id
+            b, c = t[:, :2].long().T
+            
+            gxy = t[:, 2:4]
+            gwh = t[:, 4:6]
+            
+            gij = gxy.long()
+            gi, gj = gij.T
+            
+            # anchor index
+            a = t[:, 6].long()
+            
+            # add indices
+            indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))
+            
+            # add target_box
+            tboxes.append(torch.cat((gxy - gij, gwh),dim=1))
+            
+            # add anchor
+            anch.append(anchors[a])
+            
+            # add class
+            tcls.append(c)
+            
+        return tcls, tboxes, indices, anch
